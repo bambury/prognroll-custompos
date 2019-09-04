a60b3eb9c761d9ee337f51fef325697c6c8bc029
bambury <45920408+bambury@users.noreply.github.com>
2019-09-04 16:06:35 +0200

    Position option (see comment)

--- src/prognroll.js
+++ src/prognroll.js
@@ -6,7 +6,8 @@
             height: 5, // progress bar height
             color: "#50bcb6", // progress bar background color
             custom: false, // if you make it true, you can add your custom div and see it's scroll progress on the page
-            attachTo: "body" //element to prepend the progress bar to
+            attachTo: "body", //element (.class or #id) to prepend the progress bar to
+            position: "fixed" //set to absolute when attachTo is not set to "body"
         }, options);
 
         return this.each(function () {
